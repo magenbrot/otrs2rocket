598aba79a1b829fe235100b528ecb0411987f0b0
ndom91 <yo@ndo.dev>
2020-08-14 14:12:21 +0000

    update: to dotenv instead of .secrets

--- otrs2rocket.py
+++ otrs2rocket.py
@@ -8,11 +8,16 @@
 #
 
 import json
-import os
 import pymysql
 import requests
+import os
 import sys
 import psycopg2
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv()
+
+DEBUG = False
 
 # Rocket.Chat incoming webhook URL
 #
@@ -42,20 +47,25 @@ if len(sys.argv) < 3:
     print("CLI Arguments Missing")
     sys.exit (1)
 
+INPUT = {"1": sys.argv[1], "2": sys.argv[2]}
+print(INPUT,  file=open('/opt/otrs2rocket/out.log', 'a'))
+
 if MYSQL_HOST:
+    if DEBUG:
+      print('mysql')
     conn = pymysql.connect(host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASS, db=MYSQL_DB)
     cur = conn.cursor()
     sql = "SELECT ticket.id, ticket.tn, ticket.title, CASE WHEN customer_company.name IS NOT NULL THEN customer_company.name ELSE ticket.customer_id END AS customer FROM ticket LEFT JOIN customer_company ON (ticket.customer_id = customer_company.customer_id) WHERE tn = %s"
     cur.execute(sql, sys.argv[1])
 
 if PSQL_HOST:
+    if DEBUG:
+      print('psql')
     conn = psycopg2.connect(dbname=PSQL_DB, user=PSQL_USER, password=PSQL_PASS, host=PSQL_HOST, port=PSQL_PORT)
     cur = conn.cursor()
     sql = "SELECT ticket.id, ticket.tn, ticket.title, CASE WHEN customer_company.name IS NOT NULL THEN customer_company.name ELSE ticket.customer_id END AS customer FROM ticket LEFT JOIN customer_company ON (ticket.customer_id = customer_company.customer_id) WHERE tn = %s"
-    cur.execute(sql, (sys.argv[2],))
+    cur.execute(sql, (sys.argv[1],))
 
-# print(cur)
-# print(cur.rowcount)
 
 if cur.rowcount:
     for row in cur:
@@ -68,15 +78,26 @@ else:
     # exit if no matching ticket was found
     cur.close()
     conn.close()
+    print('Err: No matching ticket found!')
     sys.exit (0)
 
 cur.close()
 conn.close()
 
+if DEBUG:
+  print(cur.rowcount)
+  print(id, tn, title, customer)
+
 headers = {'Content-type': 'application/json'}
-payload = {'text': 'New ticket #' + tn + ' from \"' + customer + '\" --> \"' + title + '\"\n' + OTRS_URL + id}
-# print(payload)
+payload = {'alias': 'OTRS Bot', 'icon_url': 'https://otrs.com/wp-content/uploads/Logo-Kreis-control-RGB-1000px-150x150.png', 'text': '##### ' + title + '\n', 'attachments': [{ 'color': '#ab4a53', 'text': ':bookmark_tabs: **#' + tn + '**\n :blond-haired_man_light_skin_tone: **' + customer + '** \n\n :globe_with_meridians:  [View in OTRS](' + OTRS_URL + id + ')'}]}
+
+if DEBUG:
+  print(payload)
+
 r = requests.post(WEBHOOK_URL, json=payload, headers=headers)
+
+if DEBUG:
+  print(r)
 
 if r.status_code != 200:
     raise ValueError('Request to Rocket.Chat returned an error %s, the response is:\n%s' % (r.status_code, r.text))
